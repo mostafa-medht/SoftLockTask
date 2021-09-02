9e3c77beb0bd9bf618330775ff1db6dc44108327
mostafa medht <mostafamedht98@gmail.com>
2021-09-02 13:07:10 +0200

    Add Key Col To Laravel User table and make it generated dynamic wih register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,6 +68,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'key' => base64_encode(openssl_random_pseudo_bytes(256)),
         ]);
+        // dd('**');
     }
 }
